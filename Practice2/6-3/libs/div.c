b672766c4f4ccc573b9e5d5597ef03fe896a96fe
santokoro <165536063+santokoro@users.noreply.github.com>
2026-08-09 18:53:46 +0700

    Add files via upload

---
+++ Practice2/6-3/libs/div.c
@@ -0,0 +1,5 @@
+#include <math.h>
+double calc(double a, double b) {
+    if (fabs(b) < 1e-12) return 0.0 / 0.0;
+    return a / b;
+}
---
+++ Practice2/6-3/libs/min.c
@@ -0,0 +1,2 @@
+#include <math.h>
+double calc(double a, double b) { return (a < b) ? a : b; }
